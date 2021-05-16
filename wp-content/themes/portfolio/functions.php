4263ac978b75485888c3dd0539f4787895c3645b
MaideAkdede <maide.akdede@student.hepl.com>
2021-05-16 23:01:29 +0200

    :pencil2: Add Menu

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -1,8 +1,62 @@
 <?php
-/* * * Return a compiled assets URI * * */
-function p_assets($path)
+/* *****
+ * Return a Menu Structure
+ * *****/
+
+function p_menu($location)
 {
-    return rtrim(get_template_directory_uri(), '/') . '/public/' . ltrim($path, '/');
+    $locations = get_nav_menu_locations();
+    $menu = $locations[$location];
+    $links = wp_get_nav_menu_items($menu);
+    $links = array_map(function($post){
+        $link = new \stdClass();
+
+        $link->url = $post->url;
+        $link->label = $post->title;
+
+        return $link;
+    }, $links);
+
+    return $links;
+
+}
+/* *****
+ * Register navigation menus
+ * *****/
+add_action('init', 'p_custom_navigation_menus');
+
+function p_custom_navigation_menus()
+{
+    register_nav_menus([
+        'main' => 'Navigation principale',
+        'footer' => 'Navigation en pied de page',
+    ]);
+}
+
+
+/* *****
+ * Register a custom post type
+ * *****/
+add_action('init', 'p_custom_post_type');
+
+function p_custom_post_type()
+{
+    register_post_type('project', [
+        'label' => 'Mes Projets',
+        'labels' => [
+            'singular_name' => 'projet',
+            'add_new' => 'Nouveau projet',
+            'add_new_item' => 'Ajouter un nouveau projet',
+        ],
+        'description' => 'Liste de tous mes projets.',
+        'public' => true,
+        'menu_position' => 2,
+        'menu_icon' => 'dashicons-editor-code',
+        'supports' => ['title', 'excerpt', 'editor'],
+        'rewrite' => [
+            'slug' => 'projets'
+        ]
+    ]);
 }
 
 /* * * disable the wordpress gutenberg editor * * */
@@ -10,4 +64,12 @@ add_filter("use_block_editor_for_post_type", "disable_gutenberg_editor");
 function disable_gutenberg_editor()
 {
     return false;
+}
+
+/* *****
+ * Return a compiled asset's URI
+ * *****/
+function p_assets($path)
+{
+    return rtrim(get_template_directory_uri(), '/') . '/public/' . ltrim($path, '/');
 }
--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -3,21 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php if (is_front_page()){
+    <title><?php if (is_front_page()) {
             echo 'Maïde Akdede • ';
             echo bloginfo('description');
         } else {
-            wp_title('Portfolio • ');
-        } ;?>
+            wp_title('Maïde Akdede • ');
+        }; ?>
     </title>
     <!--ASSETS-->
-    <link rel="stylesheet" href="<?= p_assets('css/theme.css') ?>">
-    <script src="<?= p_assets('js/app.js') ?>"></script>
+    <link rel="stylesheet" href="<?= p_assets('/css/theme.css') ?>">
     <!--WORDPRESS-->
     <?php wp_head(); ?>
 </head>
 <body>
+<canvas id="canvas"></canvas>
+<script src="<?= p_assets('/js/app.js') ?>"></script>
 <header class="top">
-    <h1 class="top__title sro"><?= is_front_page() ? bloginfo('description') . ' de Maïde Akdede • Accueil': trim(wp_title('Portfolio • ')); ?></h1>
+    <h1 class="top__title sro"><?= is_front_page() ? bloginfo('description') : trim(wp_title('Maïde Akdede • ')); ?></h1>
+    <nav class="nav">
+        <h2 class="sro">Navigation</h2>
+        <p class="nav__logo">Maïde Akdede</p>
 
+        <?php foreach (p_menu('main') as $link): ?>
+            <a href="<?= $link->url; ?>"><?= $link->label; ?></a>
+        <?php endforeach; ?>
+    </nav>
 </header>
