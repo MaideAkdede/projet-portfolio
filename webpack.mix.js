d1356622ba8c9087755f68db04f34eb070c811f2
MaideAkdede <maide.akdede@student.hepl.com>
2021-05-16 22:59:33 +0200

    :tada: init compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,8 @@
 let mix = require('laravel-mix');
 
-mix.sass('wp-content/themes/portfolio/resources/sass/theme.scss', 'wp-content/themes/portfolio/public/css')
-    .js('wp-content/themes/portfolio/resources/js/app.js', 'wp-content/themes/portfolio/public/js');
+require('laravel-mix-pluton');
+
+mix.sass('wp-content/themes/portfolio/resources/sass/theme.scss', 'wp-content/themes/portfolio/public/css');
+
+mix.pluton('wp-content/themes/portfolio/resources/js/parts')
+    .js('wp-content/themes/portfolio/resources/js/app.js', 'wp-content/themes/portfolio/public/js')
