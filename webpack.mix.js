62eef391858afda55a291c4f898bf1e906dde921
MaideAkdede <maide.akdede@student.hepl.com>
2021-05-27 00:45:16 +0200

    :construction: Webpack Update Mix Pluton

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,7 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 require('laravel-mix-pluton');
 
-mix.sass('wp-content/themes/portfolio/resources/sass/theme.scss', 'wp-content/themes/portfolio/public/css');
-
 mix.pluton('wp-content/themes/portfolio/resources/js/parts')
     .js('wp-content/themes/portfolio/resources/js/app.js', 'wp-content/themes/portfolio/public/js')
+    .sass('wp-content/themes/portfolio/resources/sass/theme.scss', 'wp-content/themes/portfolio/public/css');
